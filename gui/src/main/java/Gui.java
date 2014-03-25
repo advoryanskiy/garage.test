c6ecb0f59184937a664de15527ad17010c766d89
Aleksey Dvoryanskiy <dvoryanskiy@mercurydevelopment.com>
2014-03-25 10:50:02 -0400

    Added classes and tests

---
+++ gui/src/main/java/Gui.java
@@ -0,0 +1,11 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Date: 3/25/2014
+ * Time: 10:45 AM
+ */
+public class Gui {
+    public int printMessage() {
+        System.out.println("Test");
+        return 0;
+    }
+}
